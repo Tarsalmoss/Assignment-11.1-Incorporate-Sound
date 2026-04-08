85b44ccd76a63be28e061caa9ea0ea273ee01348
Tarsalmoss <IANDALON87862@uat.edu>
2026-04-07 18:41:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 /*
-This file controls background space sound
-All JavaScript is kept outside HTML (requirement)
+This file controls background space sound with smooth effects
+All JavaScript is external (assignment requirement)
 */
 
 // Create audio object
 const sound = new Audio("sound.mp3");
 
-// Make sound loop continuously
+// Set looping
 sound.loop = true;
+
+// Set default volume (ambient level)
+sound.volume = 0.4;
 
 // Get buttons
 const playButton = document.getElementById("playBtn");
 const pauseButton = document.getElementById("pauseBtn");
 
-// Play sound when clicked
+// Function to fade in sound
+function fadeIn(audio) {
+audio.volume = 0;
+audio.play();
+
+```
+let volume = 0;
+const fade = setInterval(function () {
+    if (volume < 0.4) {
+        volume += 0.02;
+        audio.volume = volume;
+    } else {
+        clearInterval(fade);
+    }
+}, 100);
+```
+
+}
+
+// Function to fade out sound
+function fadeOut(audio) {
+let volume = audio.volume;
+
+```
+const fade = setInterval(function () {
+    if (volume > 0.02) {
+        volume -= 0.02;
+        audio.volume = volume;
+    } else {
+        audio.pause();
+        clearInterval(fade);
+    }
+}, 100);
+```
+
+}
+
+// Play button event
 playButton.addEventListener("click", function () {
-sound.play();
+fadeIn(sound);
 });
 
-// Pause sound when clicked
+// Pause button event
 pauseButton.addEventListener("click", function () {
-sound.pause();
+fadeOut(sound);
 });
+
